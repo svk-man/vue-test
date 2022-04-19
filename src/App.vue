c9c8c94ccc52fae64607a91e1c8f3dfb2d95e839
Sergey Kravchenko <kravchenko.sgv@gmail.com>
2022-04-20 01:37:10 +0300

    feat: add user filter by id and name

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,29 @@
 <template>
-  <data-table :items=postsItems :titles=postsTitles />
+  <users-table :users=users />
 </template>
 
 <script>
 import * as Api from './api/axios';
-import DataTable from './components/DataTable.vue';
+import UsersTable from './components/UsersTable.vue';
 
 export default {
-  components: { DataTable },
+  components: { UsersTable },
   name: 'App',
   data() {
     return {
-      postsTitles: [],
-      postsItems: [],
-      photosTitles: [],
-      photosItems: [],
-      usersTitles: [],
-      usersItems: [],
+      users: [],
     };
   },
   methods: {
-    getPosts() {
-      Api.getPosts()
-        .then((posts) => {
-          this.postsTitles = ['userId', 'id', 'title', 'body'];
-          this.postsItems = posts;
-        });
-    },
     getUsers() {
       Api.getUsers()
         .then((users) => {
-          this.usersTitles = ['id', 'name', 'username', 'email', 'city'];
-          this.usersItems = users.map((user) => (
-            {
-              id: user.id,
-              name: user.name,
-              username: user.username,
-              email: user.email,
-              city: user.address.city,
-            }));
-        });
-    },
-    getPhotos() {
-      Api.getPhotos()
-        .then((photos) => {
-          this.photosTitles = ['id', 'title', 'url'];
-          this.photosItems = photos.map((photo) => (
-            {
-              id: photo.id,
-              title: photo.title,
-              url: photo.url,
-            }));
+          this.users = users;
         });
     },
   },
   mounted() {
-    this.getPosts();
     this.getUsers();
-    this.getPhotos();
   },
 };
 </script>
---
+++ src/components/UsersTable.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <label for="id">
+      <input v-model.number="idFilter" type="text" placeholder="id" name="id" />
+    </label>
+    <label for="name">
+      <input v-model="nameFilter" type="text" placeholder="name" name="name" />
+    </label>
+  </div>
+  <table>
+    <thead>
+      <tr>
+        <th>id</th>
+        <th>name</th>
+        <th>username</th>
+        <th>email</th>
+        <th>city</th>
+        <th>phone</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="user in filteredUsers" :key="user.id">
+        <td>{{  user.id }}</td>
+        <td>{{  user.name }}</td>
+        <td>{{  user.username }}</td>
+        <td>{{  user.email }}</td>
+        <td>{{  user.address.city }}</td>
+        <td>{{  user.phone }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: 'UsersTable',
+  data() {
+    return {
+      idFilter: '',
+      nameFilter: '',
+    };
+  },
+  computed: {
+    filteredUsers() {
+      return this.users
+        .filter((user) => {
+          const isIdExist = user.id === this.idFilter;
+          const isFilterPossible = typeof this.idFilter === 'number';
+          return isFilterPossible ? isIdExist : true;
+        })
+        .filter((user) => {
+          const isFilterPossible = this.nameFilter.trim() !== '';
+          const isNameExist = user.name.toLowerCase().indexOf(this.nameFilter.toLowerCase()) !== -1;
+          return isFilterPossible ? isNameExist : true;
+        });
+    },
+  },
+  props: {
+    users: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped lang='scss'></style>
