106a4a7c0c9f44dbbf4d1b1eb9448cbb8dd5b52d
Sergey Kravchenko <kravchenko.sgv@gmail.com>
2022-04-21 04:47:10 +0300

    feat: build project

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/dist/'
+    : '/',
   configureWebpack: {
     module: {
       rules: [
