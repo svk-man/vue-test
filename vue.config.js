b4fbb32fc6afc97463a019919e43e1f2d352a266
Sergey Kravchenko <kravchenko.sgv@gmail.com>
2022-04-21 04:50:37 +0300

    fix: fix publicPath

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/dist/'
+    ? '/vue-test/'
     : '/',
   configureWebpack: {
     module: {
